a012a370a560b5820e7ea3200579bb8b0c40b59d
Arthur Oliveira Viana <46693700+VianaArthur@users.noreply.github.com>
2020-03-22 12:38:51 -0300

    Improvements
    Change the behavior of game, now play button runs a function.
    
    Change icon color.
    
    Change title.

--- js/index.js
+++ js/index.js
@@ -1,23 +1,24 @@
-let randomNumber1 = Math.floor(Math.random() * 6) + 1; // return numbers from 1-6
+const playButton = document.getElementById("playButton");
 
-let randomImageSource1 = "images/dice" + randomNumber1 + ".png";
+playButton.addEventListener("click", playDice);
 
-document.getElementsByClassName("img1")[0].setAttribute("src", randomImageSource1);
-
-
-let randomNumber2 = Math.floor(Math.random() * 6) + 1;
-
-let randomImageSource2 = "images/dice" + randomNumber2 + ".png";
-
-document.getElementsByClassName("img2")[0].setAttribute("src", randomImageSource2);
-
-
-if (randomNumber1 > randomNumber2) { //If player 1 wins
-  document.querySelector("h1").innerHTML = "🚩 Player 1 Wins!";
-}
-else if (randomNumber2 > randomNumber1) { // if player 2 wins
-  document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
-}
-else { //draw if dices are equal
-  document.querySelector("h1").innerHTML = "Draw!";
+function playDice (){
+  let randomNumber1 = Math.floor(Math.random() * 6) + 1; // return numbers from 1-6
+  let randomImageSource1 = "images/dice" + randomNumber1 + ".png";
+  document.getElementsByClassName("img1")[0].setAttribute("src", randomImageSource1);
+  
+  let randomNumber2 = Math.floor(Math.random() * 6) + 1;
+  let randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+  document.getElementsByClassName("img2")[0].setAttribute("src", randomImageSource2);
+  
+  // Check who wins
+  if (randomNumber1 > randomNumber2) { //If player 1 wins
+    document.querySelector("h1").innerHTML = "🚩 Player 1 Wins!";
+  }
+  else if (randomNumber2 > randomNumber1) { // if player 2 wins
+    document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
+  }
+  else { //draw if dices are equal
+    document.querySelector("h1").innerHTML = "Draw!";
+  }
 }
